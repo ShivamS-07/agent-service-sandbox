ce63531e168598fcc6c249aa81f05c14a107070f
kedasong-gbi <101576854+KedaSong@users.noreply.github.com>
2024-06-05 16:12:30 -0400

    drop time interval for worklog events (#174)

--- agent_service/planner/constants.py
+++ agent_service/planner/constants.py
@@ -10,7 +10,7 @@ CREATE_EXECUTION_PLAN_FLOW_NAME = "create_execution_plan"
 INITIAL_PLAN_TRIES = 3
 EXECUTION_TRIES = 3
 
-WORKLOG_INTERVAL = 2  # every 2 seconds send a worklog event to FE
+WORKLOG_INTERVAL = 0.5  # every X seconds send a worklog event to FE
 
 
 class Action(str, Enum):
