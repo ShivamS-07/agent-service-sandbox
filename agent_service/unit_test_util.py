a7c2e2c279f2fe1e6d04a983d1148fc655144e11
David Grohmann <104018429+davidgrohmann@users.noreply.github.com>
2024-09-17 19:04:36 -0400

    remove changes related to test detection that arent working correctly (#975)
    * remove changes related to test detection that arent working correctly
    
    * format
    
    * more

--- agent_service/unit_test_util.py
+++ agent_service/unit_test_util.py
@@ -1,6 +1,5 @@
 import inspect
 import logging
-import sys
 from typing import Any
 
 logger = logging.getLogger(__name__)
@@ -13,15 +12,15 @@ def _super_print(*args: Any) -> None:
 
 def _check_if_test() -> bool:
 
-    for key in ["unittest_parallel", "pytest"]:
-        if key in sys.modules:
-            _super_print("\n\n================ TESTING DETECTED!!! ====================")
-            _super_print("module", key, "detected")
-            _super_print("================ TESTING DETECTED!!! ====================\n\n")
-            return True
-
+    # check the stack to see if any unit-test like directories exist
     for frame in inspect.stack():
-        for key in ["service/tests/", "service/regression_test", "unittest", "pytest"]:
+        for key in [
+            "service/tests/",
+            "service/regression_test/",
+            "/unittest/",
+            "/pytest/",
+            "/_pytest/",
+        ]:
             if key in frame.filename:
                 _super_print("\n\n================ TESTING DETECTED!!! ====================")
                 _super_print("key:", key, "found in frame:", frame)
