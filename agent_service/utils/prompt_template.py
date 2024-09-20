73c7d5d746364a00ed6de16eb21720e1d46b7111
MohammadZ <168458527+iammohammadzarei@users.noreply.github.com>
2024-09-20 08:55:16 -0400

    Template preview - Add graph types (#990)
    * first
    
    * fix endpoints
    
    * fix endpoints
    
    * add tp app.py
    
    * Fix bug
    
    * add output types
    
    * fix bug
    
    * cleaning
    
    * fix commentary prompt
    
    * format
    
    * remove print
    
    * [build-me]
    
    * use httpexeption when plan is none
    
    * minor fixes
    
    * no acync for _output_types_from_plan
    
    * add graph types to OutputType

--- agent_service/utils/prompt_template.py
+++ agent_service/utils/prompt_template.py
@@ -5,15 +5,16 @@ from uuid import uuid4
 
 from pydantic import BaseModel, Field
 
+from agent_service.io_types.graph import GraphType
 from agent_service.planner.planner_types import ExecutionPlan
 
 
 class OutputType(enum.StrEnum):
     TEXT = "text"
     TABLE = "table"
-    LINE_GRAPH = "line_graph"
-    PIE_GRAPH = "pie_graph"
-    BAR_GRAPH = "bar_graph"
+    LINE_GRAPH = GraphType.LINE
+    PIE_GRAPH = GraphType.PIE
+    BAR_GRAPH = GraphType.BAR
 
 
 class PromptTemplate(BaseModel):
