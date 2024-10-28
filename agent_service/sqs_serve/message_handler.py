cc25b952661807e625c2e313e6458b8d2b417956
Zach <zach.silver@gradientboostedinvestments.com>
2024-10-28 14:32:24 -0400

    Add logs to track startup time for python process (#1265)

--- agent_service/sqs_serve/message_handler.py
+++ agent_service/sqs_serve/message_handler.py
@@ -1,8 +1,14 @@
+import logging
+import os
 from typing import Dict
 
-from prefect import tags
+from dateutil.parser import parse
 
 from agent_service.planner.executor import create_execution_plan, run_execution_plan
+from agent_service.utils.date_utils import get_now_utc
+from agent_service.utils.event_logging import log_event
+
+logger = logging.getLogger(__name__)
 
 
 class MessageHandler:
@@ -10,15 +16,35 @@ class MessageHandler:
     async def handle_message(self, message: Dict) -> Dict:
         method = message.get("method")
         arguments = message["arguments"]
+        pod_start = os.getenv("STARTED_AT")
+        pod_start_dt = None
+        if pod_start:
+            try:
+                pod_start_dt = parse(pod_start)
+            except Exception:
+                logger.exception("Unable to parse STARTED_AT env var")
+        now = get_now_utc()
         if method == "run_execution_plan":
-            agent_id = arguments["context"]["agent_id"]
-            with tags(agent_id):
-                await run_execution_plan(**arguments)
-                return message
+            log_event(
+                "agent-svc-run-execution-plan-start",
+                event_data={
+                    "pod_start": pod_start_dt,
+                    "execution_start": now,
+                    "arguments": arguments,
+                },
+            )
+            await run_execution_plan(**arguments)
+            return message
         elif method == "create_execution_plan":
-            agent_id = arguments["agent_id"]
-            with tags(agent_id):
-                await create_execution_plan(**arguments)
-                return message
+            log_event(
+                "agent-svc-create-execution-plan-start",
+                event_data={
+                    "pod_start": pod_start_dt,
+                    "execution_start": now,
+                    "arguments": arguments,
+                },
+            )
+            await create_execution_plan(**arguments)
+            return message
         else:
             raise NotImplementedError(f"Method {method} is not supported")
