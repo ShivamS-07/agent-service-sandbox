28034ce2dcfca61ca2e110b9a26d317c1df6da54
kedasong-gbi <101576854+KedaSong@users.noreply.github.com>
2024-11-21 16:07:32 -0500

    mute event_data for /health (#1495)

--- application.py
+++ application.py
@@ -346,7 +346,7 @@ class ProcessTimeMiddleware:
         self.app = app
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] != "http":
+        if scope["type"] != "http" or scope.get("path") == "/api/health":
             await self.app(scope, receive, send)
             return
 
