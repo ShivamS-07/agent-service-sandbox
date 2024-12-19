7495b1f390b5c9e4962b80789deb0fc239610d5e
Tommy Eberle <105869028+teberleboosted@users.noreply.github.com>
2024-12-19 09:53:58 -0600

    [WC-2564] Add brightdata costs to daily cost report (#1707)
    We already log the daily LLM costs into the #llm-costs slack channel. We
    now have the brightdata cost information in clickhouse, so let's start
    logging that as part of the daily report as well.

--- scripts/notify_llm_costs.py
+++ scripts/notify_llm_costs.py
@@ -9,31 +9,66 @@ from agent_service.utils.clickhouse import Clickhouse
 logger = logging.getLogger(__name__)
 
 
-async def main() -> None:
+async def get_llm_costs_str() -> str:
     ch_dev = Clickhouse(environment="DEV")
     ch_prod = Clickhouse(environment="ALPHA")
 
-    cost_sql = """
-    SELECT round(sum(cost_usd), 2) as total_cost_usd
-    FROM llm.queries
-    WHERE timestamp >= toStartOfDay(yesterday())
-    AND timestamp < toStartOfDay(today())
-    """
+    llm_cost_sql = """
+        SELECT round(sum(cost_usd), 2) as total_cost_usd
+        FROM llm.queries
+        WHERE timestamp >= toStartOfDay(yesterday())
+        AND timestamp < toStartOfDay(today())
+        """
 
-    dev_cost_yesterday = (await ch_dev.generic_read(sql=cost_sql))[0]["total_cost_usd"]
-    prod_cost_yesterday = (await ch_prod.generic_read(sql=cost_sql))[0]["total_cost_usd"]
+    dev_cost_yesterday = (await ch_dev.generic_read(sql=llm_cost_sql))[0]["total_cost_usd"]
+    prod_cost_yesterday = (await ch_prod.generic_read(sql=llm_cost_sql))[0]["total_cost_usd"]
 
     total_cost_yesterday = dev_cost_yesterday + prod_cost_yesterday
     date_yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
     msg_content = (
-        f"Total cost for {date_yesterday} was ${total_cost_yesterday}:\n"
+        f"Total LLM cost for {date_yesterday} was ${total_cost_yesterday}:\n"
         f"    Dev cost: ${dev_cost_yesterday}\n"
         f"    Prod cost: ${prod_cost_yesterday}\n"
         f"For detailed breakdown of cost, visit this link:\n"
         f"http://grafana.boosted.ai/d/edwbon46t3hfkc/cost-monitoring?from=now-1d%2Fd&to=now-1d%2Fd&orgId=1&tab=query"
     )
+    return msg_content
 
-    SlackSender("llm-costs").send_message(msg_content, int(time.time()) + 60)
+
+async def get_brightdata_costs_str() -> str:
+    ch_dev = Clickhouse(environment="DEV")
+    ch_prod = Clickhouse(environment="ALPHA")
+
+    bright_data_cost_sql = """
+        SELECT round(sum(cost_usd), 2) as total_cost_usd
+        FROM agent.brightdata_requests
+        WHERE timestamp >= toStartOfDay(yesterday())
+        AND timestamp < toStartOfDay(today())
+        """
+
+    dev_cost_yesterday = (await ch_dev.generic_read(sql=bright_data_cost_sql))[0]["total_cost_usd"]
+    prod_cost_yesterday = (await ch_prod.generic_read(sql=bright_data_cost_sql))[0][
+        "total_cost_usd"
+    ]
+
+    total_cost_yesterday = dev_cost_yesterday + prod_cost_yesterday
+    date_yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+    msg_content = (
+        f"Total Brightdata cost for {date_yesterday} was ${total_cost_yesterday}:\n"
+        f"    Dev cost: ${dev_cost_yesterday}\n"
+        f"    Prod cost: ${prod_cost_yesterday}\n"
+        f"For detailed breakdown of cost, visit this link:\n"
+        f"http://grafana.boosted.ai/d/edwbon46t3hfkc/cost-monitoring?from=now-1d%2Fd&to=now-1d%2Fd&orgId=1&tab=query"
+    )
+    return msg_content
+
+
+async def main() -> None:
+    llm_cost_str = await get_llm_costs_str()
+    brighdata_cost_str = await get_brightdata_costs_str()
+
+    SlackSender("llm-costs").send_message(llm_cost_str, int(time.time()) + 60)
+    SlackSender("llm-costs").send_message(brighdata_cost_str, int(time.time()) + 60)
 
 
 if __name__ == "__main__":
