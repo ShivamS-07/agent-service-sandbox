8bda0de808b740cc6ca2c83d30abc0983acc4d86
Joshua Cheng <josh510cheng@gmail.com>
2025-01-06 14:49:51 -0500

    [WC-2544] develop revamp sqs handler logic for initial run execution plans (#1781)
    * Initial test - write file with meta param on replacement
    
    * Add replacement queue spawner script
    
    * Instead use message IDs given by invoker
    
    * Add parent id to script for logging
    
    * Add temp artifacts
    
    * Add cron support by writing to db when no longe ridle
    
    * Temp - add queue for local testing
    
    * Update json to str
    
    * Fix - clean up of message id
    
    * Add new log line to test
    
    * Clean up testing artifacts
    
    * Mypy

--- scripts/run_plan_sqs_handler.py
+++ scripts/run_plan_sqs_handler.py
@@ -1,13 +1,13 @@
 import argparse
 import asyncio
+import datetime
 import json
 import logging
 import signal
 import traceback
-import uuid
 from datetime import timedelta
 from enum import Enum
-from typing import Any
+from typing import Any, Dict
 
 import boto3
 
@@ -16,15 +16,12 @@ from agent_service.sqs_serve.message_handler import MessageHandler
 from agent_service.utils.constants import AGENT_RUN_EXECUTION_PLAN_QUEUE, BOOSTED_DAG_QUEUE
 from agent_service.utils.date_utils import get_now_utc
 from agent_service.utils.event_logging import log_event
+from agent_service.utils.postgres import Postgres
 from agent_service.utils.s3_upload import download_json_from_s3
 
 LOGGER = logging.getLogger(__name__)
 DEFAULT_IDLE_TTL = 10
-
-
-class ReplacementReasons(Enum):
-    NO_MESSAGE = "no_message"
-    MESSAGE_RECEIVED = "message_received"
+EVENT_NAME = "agent_launch_sqs_handler_replacement"
 
 
 class MessageTypes(Enum):
@@ -41,7 +38,73 @@ class GracefulSigterm:
         self.kill_now = True
 
 
-async def poll_sqs_for_time(idle_ttl: int, outbound_queue: str) -> None:
+def replace_and_set_metadata(
+    sqs: Any,
+    queue_url: str,
+    message_id: str,
+    parent_message_id: str,
+    idle_ttl: int,
+    spawn_time: datetime.date,
+    event_name: str,
+    message: Dict = {},
+) -> None:
+    """
+    Sends a replacement SQS message and logs the event.
+    Also writes to PG that this job is no longer idle
+    """
+    # Send SQS message to self replace
+    boosted_dag_message = {
+        "method": "AgentServiceKickoffSQSHandler",
+        "idle_ttl": idle_ttl,
+        "message_id": message_id,
+        "parent_message_id": parent_message_id,
+    }
+    sqs.send_message(
+        QueueUrl=queue_url,
+        DelaySeconds=1,
+        MessageBody=json.dumps(boosted_dag_message),
+    )
+
+    # Update that this job is no longer idle, add in the extra info
+    # For tracking if we have it.
+    upsert = {
+        "message_id": message_id,
+        "parent_message_id": parent_message_id,
+        "listening": False,
+        "agent_id": message.get("agent_id"),
+        "plan_id": message.get("plan_id"),
+        "plan_run_id": message.get("plan_run_id"),
+        "user_id": message.get("user_id"),
+        "message": json.dumps(message),
+    }
+    pg = Postgres()
+    pg.generic_insert_or_update(
+        table_name="agent.sqs_handler_logs",
+        values_to_insert=upsert,
+        conflict_columns=["message_id"],
+        columns_to_update=list(upsert.keys()),
+    )
+
+    log_event(
+        event_name=event_name,
+        event_data={
+            "start_time_utc": spawn_time.isoformat(),
+            "end_time_utc": get_now_utc().isoformat(),
+            "message": boosted_dag_message,
+            "type": MessageTypes.MESSAGE_SENT.value,
+            "message_id": message_id,
+        },
+    )
+
+
+async def poll_sqs_for_time(
+    message_id: str, parent_message_id: str, idle_ttl: int, outbound_queue: str
+) -> None:
+    """
+    Listener job for run_execution_plan messages. Spins up a replacement when a message is received.
+    Else will exit after the idle time to live period passes, also spinning up a replacement before
+    exiting.
+    """
     queue_url = AGENT_RUN_EXECUTION_PLAN_QUEUE
     message_handler = MessageHandler()
 
@@ -49,11 +112,11 @@ async def poll_sqs_for_time(idle_ttl: int, outbound_queue: str) -> None:
         raise Exception("Environment variable AGENT_RUN_EXECUTION_PLAN_QUEUE must be set.")
     sqs = boto3.client("sqs", region_name="us-west-2")
 
+    LOGGER.info(f"Now live, SQS listener job with id: {message_id}, parent id: {parent_message_id}")
     LOGGER.info(f"Listening to queue {queue_url}")
     graceful_sigterm = GracefulSigterm()
 
     spawn_time = get_now_utc()
-    message_id = str(uuid.uuid4())
 
     boosted_dag_queue = sqs.get_queue_url(QueueName=outbound_queue)
     boosted_dag_queue_url = boosted_dag_queue["QueueUrl"]
@@ -62,26 +125,14 @@ async def poll_sqs_for_time(idle_ttl: int, outbound_queue: str) -> None:
         # Check if idle_ttl has been exceeded, if so spawn a replacement and exit
         if (get_now_utc() - spawn_time) > timedelta(minutes=idle_ttl):
             LOGGER.info(f"No messages received for {idle_ttl} minutes. Exiting due to idle TTL.")
-            LOGGER.info(f"Creating replacement workflow to queue: {boosted_dag_queue=}")
-            boosted_dag_message = {
-                "method": "AgentServiceKickoffSQSHandler",
-                "idle_ttl": idle_ttl,
-                "reason": ReplacementReasons.NO_MESSAGE.value,
-            }
-            sqs.send_message(
-                QueueUrl=boosted_dag_queue_url,
-                DelaySeconds=1,
-                MessageBody=json.dumps(boosted_dag_message),
-            )
-            log_event(
-                event_name="agent_launch_sqs_handler_replacement",
-                event_data={
-                    "start_time_utc": spawn_time.isoformat(),
-                    "end_time_utc": get_now_utc().isoformat(),
-                    "message": boosted_dag_message,
-                    "type": MessageTypes.MESSAGE_SENT.value,
-                    "message_id": message_id,
-                },
+            replace_and_set_metadata(
+                sqs,
+                boosted_dag_queue_url,
+                message_id,
+                parent_message_id,
+                idle_ttl,
+                spawn_time,
+                EVENT_NAME,
             )
             exit(0)
 
@@ -102,29 +153,18 @@ async def poll_sqs_for_time(idle_ttl: int, outbound_queue: str) -> None:
             sqs_message = message["Body"]
             LOGGER.info(f"Received Message: {sqs_message}")
             message_dict = json.loads(sqs_message)
-            message_id = message_dict.get("message_id", message_id)
 
             # First, before processing send a message to boosted-dag to create a replacement
-            LOGGER.info(f"Creating replacement workflow to queue: {boosted_dag_queue=}")
-            boosted_dag_message = {
-                "method": "AgentServiceKickoffSQSHandler",
-                "idle_ttl": idle_ttl,
-                "reason": ReplacementReasons.MESSAGE_RECEIVED.value,
-            }
-            sqs.send_message(
-                QueueUrl=boosted_dag_queue_url,
-                DelaySeconds=1,
-                MessageBody=json.dumps(boosted_dag_message),
-            )
-            log_event(
-                event_name="agent_launch_sqs_handler_replacement",
-                event_data={
-                    "start_time_utc": spawn_time.isoformat(),
-                    "end_time_utc": get_now_utc().isoformat(),
-                    "message": boosted_dag_message,
-                    "type": MessageTypes.MESSAGE_SENT.value,
-                    "message_id": message_id,
-                },
+            LOGGER.info(f"Creating replacement workflow to queue: {queue_url}")
+            replace_and_set_metadata(
+                sqs,
+                boosted_dag_queue_url,
+                message_id,
+                parent_message_id,
+                idle_ttl,
+                spawn_time,
+                EVENT_NAME,
+                message=message_dict,
             )
 
             start_time_utc = get_now_utc().isoformat()
@@ -182,6 +222,18 @@ def parse_args() -> argparse.Namespace:
         description="Create an SQS Handler for run_execution_plan agent jobs"
     )
     parser.add_argument(
+        "--message-id",
+        type=str,
+        required=True,
+        help="Internal ID of the message",
+    )
+    parser.add_argument(
+        "--parent-message-id",
+        type=str,
+        default="none",
+        help="Internal ID of the parent of the current message",
+    )
+    parser.add_argument(
         "--idle-ttl", type=int, default=10, help="How long to run without receiving a message"
     )
     parser.add_argument(
@@ -200,4 +252,6 @@ if __name__ == "__main__":
         force=True,
     )
     args = parse_args()
-    asyncio.run(poll_sqs_for_time(args.idle_ttl, args.queue))
+    asyncio.run(
+        poll_sqs_for_time(args.message_id, args.parent_message_id, args.idle_ttl, args.queue)
+    )
